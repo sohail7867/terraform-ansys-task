f440c130cb3003ce6b2791de968f26148260f369
Sohail Mohammed <sohail.mohammed@quortus.com>
2025-04-02 20:40:40 +0100

    Adding AWS intenet gateway, route tasble and route table associations

--- main.tf
+++ main.tf
@@ -32,3 +32,23 @@ resource "aws_subnet" "private_subnet" {
   availability_zone      = var.availability_zones[count.index]
   map_public_ip_on_launch = false
 }
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route" "internet_access" {
+  route_table_id         = aws_route_table.public_route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main.id
+}
+
+resource "aws_route_table_association" "public_association" {
+  count          = length(var.public_subnets)
+  subnet_id      = aws_subnet.public_subnet[count.index].id
+  route_table_id = aws_route_table.public_route_table.id
+}
