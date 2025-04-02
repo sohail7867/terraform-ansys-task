c69f73969b26f9b1d96b7f82ead21c26076c23c7
Sohail Mohammed <sohail.mohammed@quortus.com>
2025-04-02 20:38:33 +0100

    adding aws public amnd private subnet resources

--- main.tf
+++ main.tf
@@ -16,3 +16,19 @@ resource "aws_vpc" "main" {
   enable_dns_support   = true
   enable_dns_hostnames = true
 }
+
+resource "aws_subnet" "public_subnet" {
+  count                  = length(var.public_subnets)
+  vpc_id                 = aws_vpc.main.id
+  cidr_block             = var.public_subnets[count.index]
+  availability_zone      = var.availability_zones[count.index]
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "private_subnet" {
+  count                  = length(var.private_subnets)
+  vpc_id                 = aws_vpc.main.id
+  cidr_block             = var.private_subnets[count.index]
+  availability_zone      = var.availability_zones[count.index]
+  map_public_ip_on_launch = false
+}
