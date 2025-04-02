1f9b256df3c7ff22dd849d57f025cf3ff05c60a9
Sohail Mohammed <sohail.mohammed@quortus.com>
2025-04-02 20:50:47 +0100

    adding varibles.tf alomg with variables required for vpc and public and private subnets

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnets" {
+  description = "List of CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+
+variable "private_subnets" {
+  description = "List of CIDR blocks for private subnets"
+  type        = list(string)
+  default     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+}
