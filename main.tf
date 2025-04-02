7229f8a22a5fa3ea02998c8ef8217bd643290390
Sohail Mohammed <sohail.mohammed@quortus.com>
2025-04-02 20:44:08 +0100

    adding aws ec2 instance along with nginx setup

--- main.tf
+++ main.tf
@@ -52,3 +52,25 @@ resource "aws_route_table_association" "public_association" {
   subnet_id      = aws_subnet.public_subnet[count.index].id
   route_table_id = aws_route_table.public_route_table.id
 }
+
+resource "aws_instance" "nginx" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  subnet_id              = aws_subnet.public_subnet[0].id
+  associate_public_ip_address = true
+  key_name               = var.key_name
+  security_groups        = [aws_security_group.web_sg.id]
+
+  user_data = <<-EOT
+              #!/bin/bash
+              sudo yum update -y
+              sudo amazon-linux-extras enable nginx1
+              sudo yum install nginx -y
+              sudo systemctl start nginx
+              sudo systemctl enable nginx
+              EOT
+
+  tags = {
+    Name = "Nginx-Instance"
+  }
+}
