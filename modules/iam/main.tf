c10a05d5797258fd48d9150b1798bae6ade449b4
Sohail Mohammed <sohail.mohammed@quortus.com>
2025-04-04 16:06:18 +0100

    Adding and updating IAM role and instance profile for EC2 instance access

---
+++ modules/iam/main.tf
@@ -0,0 +1,21 @@
+resource "aws_iam_role" "ec2_role" {
+  name = var.role_name
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = var.instance_profile_name
+  role = aws_iam_role.ec2_role.name
+}
