3da9e0465c6e63e25528faa9221093c36ff12996
sohail7867 <38286573+sohail7867@users.noreply.github.com>
2025-04-04 15:52:37 +0100

    Updating variables.tf input variables for VPC, subnets, EC2 instance, and security group configuration

--- variables.tf
+++ variables.tf
@@ -1,47 +1,71 @@
 variable "region" {
-  description = "AWS region to deploy resources"
+  description = "The AWS region to create resources in."
   type        = string
-  default     = "us-east-1"
+  default     = "us-west-2"
 }
 
 variable "vpc_cidr" {
-  description = "CIDR block for the VPC"
+  description = "The CIDR block for the VPC."
   type        = string
   default     = "10.0.0.0/16"
 }
 
-variable "public_subnets" {
-  description = "List of CIDR blocks for public subnets"
+variable "vpc_name" {
+  description = "The name of the VPC."
+  type        = string
+  default     = "my-vpc"
+}
+
+variable "azs" {
+  description = "List of availability zones to be used for subnets."
+  type        = list(string)
+  default     = ["us-west-2a", "us-west-2b", "us-west-2c"]
+}
+
+variable "public_subnet_cidrs" {
+  description = "List of CIDR blocks for the public subnets."
   type        = list(string)
   default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
 }
 
-variable "private_subnets" {
-  description = "List of CIDR blocks for private subnets"
+variable "private_subnet_cidrs" {
+  description = "List of CIDR blocks for the private subnets."
   type        = list(string)
   default     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
 }
 
 variable "instance_type" {
-  description = "Instance type for EC2"
+  description = "The EC2 instance type to provision."
   type        = string
   default     = "t2.micro"
 }
 
-variable "ami_id" {
-  description = "AMI ID to use for EC2 instances"
-  type        = string
-  default     = "ami-00a929b66ed6e0de6" # Amazon Linux 2 AMI for us-east-1
-}
-
 variable "key_name" {
-  description = "Name of the EC2 key pair"
+  description = "The name of the SSH key pair to use for EC2 instances."
   type        = string
   default     = "teraform-aws-nginx"
 }
 
-variable "availability_zones" {
-  description = "List of availability zones for subnet creation"
+variable "ami_id" {
+  description = "The AMI ID for the EC2 instance."
+  type        = string
+  default     = "ami-087f352c165340ea1"
+}
+
+variable "instance_name" {
+  description = "The name tag for the EC2 instance."
+  type        = string
+  default     = "nginx-instance"
+}
+
+variable "allowed_http_ip_cidr" {
+  description = "CIDR block for allowed HTTP IP addresses"
   type        = list(string)
-  default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  default     = ["0.0.0.0/0"]
+}
+
+variable "allowed_ssh_ip_cidr" {
+  description = "CIDR block for allowed SSH IP addresses"
+  type        = list(string)
+  default     = ["80.5.85.5/32"]
 }
