ad79096d0c61e064f500e95997d8d7329535fa13
Sohail Mohammed <sohail.mohammed@quortus.com>
2025-04-04 16:40:53 +0100

    adding iam role variables required for module into output.tf and  varibles.tf

---
+++ modules/iam/variables.tf
@@ -0,0 +1,9 @@
+variable "role_name" {
+  description = "Name of the IAM role"
+  type        = string
+}
+
+variable "instance_profile_name" {
+  description = "Name of the IAM instance profile"
+  type        = string
+}
