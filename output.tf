31dea814174e26a2cffa0d53c0b6f34d52b2ca43
sohail7867 <38286573+sohail7867@users.noreply.github.com>
2025-04-04 15:53:57 +0100

    Updating output.tf module-based output variables for VPC, subnets, gateways, and EC2 instance

--- output.tf
+++ output.tf
@@ -1,3 +1,29 @@
-output "ec2_public_ip" {
-  value = aws_instance.nginx.public_ip
+output "vpc_id" {
+  description = "The ID of the VPC."
+  value       = module.vpc.vpc_id
+}
+
+output "public_subnet_ids" {
+  description = "The IDs of the public subnets."
+  value       = module.subnets.public_subnet_ids
+}
+
+output "private_subnet_ids" {
+  description = "The IDs of the private subnets."
+  value       = module.subnets.private_subnet_ids
+}
+
+output "internet_gateway_id" {
+  description = "The ID of the internet gateway."
+  value       = module.internet_gateway.igw_id
+}
+
+output "nat_gateway_id" {
+  description = "The ID of the NAT gateway."
+  value       = module.nat_gateway.nat_id
+}
+
+output "public_ip" {
+  description = "The public IP of the EC2 instance."
+  value       = module.ec2.public_ip
 }
